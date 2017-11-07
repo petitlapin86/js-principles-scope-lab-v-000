569f8849622fa95436f10929e79edc4c2ccff350
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 21:03:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName () {
+return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer () {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "sally";
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = "kelly";
+}
+
+let favoriteCustomer = "molly";
+
+function attemptTwoFavoriteCustomers () {
+    let favoriteCustomer = "megan";
+    let favoriteCustomer = "molly";
+}
